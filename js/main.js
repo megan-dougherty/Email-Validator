b24744d2a38f78307010c1c00e6d66dac94562e4
Megan Dougherty <megandoughertylfl@gmail.com>
2017-04-17 18:52:25 -0700

    commit

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
+
+
 function validateEmail() {
 	var emailInput = document.getElementById('emailInput').value
 	var regex = /^[a-z0-9._%-]+@[a-z0-9.-]+\.[a-z]{2,5}$/
 	console.log(regex.test(emailInput))
-}
+
+	if(emailInput == '')
+		results.textContent = "You forgot to enter a valid email address"
+}	
+	if(emailInput == 'regex')
+		results.textContent = "Success"
 
 document.getElementById("button").addEventListener("click", validateEmail,false)
